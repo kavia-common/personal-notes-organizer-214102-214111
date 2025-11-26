522cb550fac31c2504447ec8a542ac2add593fa9
Kavia AI <production@kavia.ai>
2025-11-26 18:28:52 +0000

    Checkpoint 6 - cg92c02485

--- notes_app_frontend/src/index.js
+++ notes_app_frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './theme.css';
+import './oldNotebook.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
